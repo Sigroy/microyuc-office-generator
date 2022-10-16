d66f3ce91b2be5cef738f2b190c183b6203cd6a3
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-10-16 03:16:35 -0500

    Refactor database schema structure

--- database/database.sql
+++ database/database.sql
@@ -1,65 +1,80 @@
 CREATE TABLE carta
 (
-    id                      INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    fecha_creacion          TIMESTAMP     NOT NULL,
-    fecha_visita            VARCHAR(255) DEFAULT '',
-    numero_expediente       VARCHAR(255)  NOT NULL,
-    nombre_cliente          VARCHAR(255)  NOT NULL,
-    calle                   VARCHAR(255)  NOT NULL,
-    cruzamientos            VARCHAR(255)  NOT NULL,
-    numero_direccion        VARCHAR(255)  NOT NULL,
-    colonia_fraccionamiento VARCHAR(255)  NOT NULL,
-    localidad               VARCHAR(255)  NOT NULL,
-    municipio               VARCHAR(255)  NOT NULL,
-    fecha_firma             DATE          NOT NULL,
-    documentacion           TEXT,
-    comprobacion_monto      FLOAT(200, 2) NOT NULL,
-    comprobacion_tipo       VARCHAR(255)  NOT NULL,
-    pagos_fecha_inicial     VARCHAR(255)  NOT NULL,
-    pagos_fecha_final       VARCHAR(255)  NOT NULL,
-    modalidad               VARCHAR(255)  NOT NULL,
-    tipo_credito            VARCHAR(255)  NOT NULL,
-    fecha_otorgamiento      DATE          NOT NULL,
-    monto_inicial           FLOAT(200, 2) NOT NULL,
-    mensualidades_vencidas  INT           NOT NULL,
-    adeudo_total            FLOAT(200, 2) NOT NULL,
-    nombre_archivo          VARCHAR(255)
-);
+    id                      INT(11)                                 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fecha_creacion          TIMESTAMP                               NOT NULL,
+    fecha_visita            DATETIME                                     DEFAULT NULL,
+    numero_expediente       VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    nombre_cliente          VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    calle                   VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    cruzamientos            VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    numero_direccion        VARCHAR(100) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    colonia_fraccionamiento VARCHAR(100) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    localidad               VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    municipio               VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    fecha_firma             DATE                                    NOT NULL,
+    documentacion           TEXT COLLATE utf8mb4_unicode_ci         NULL DEFAULT '',
+    comprobacion_monto      DECIMAL(30, 2)                          NOT NULL,
+    comprobacion_tipo       VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    pagos_fecha_inicial     DATE                                    NOT NULL,
+    pagos_fecha_final       DATE                                    NOT NULL,
+    modalidad               VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    tipo_credito            VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    fecha_otorgamiento      DATE                                    NOT NULL,
+    monto_inicial           DECIMAL(30, 2)                          NOT NULL,
+    mensualidades_vencidas  INT(11)                                 NOT NULL,
+    adeudo_total            DECIMAL(30, 2)                          NOT NULL,
+    nombre_archivo          VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE bitacora
 (
-    id                              INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    fecha_creacion                  TIMESTAMP    NOT NULL,
-    acreditado_nombre               VARCHAR(255) NOT NULL,
-    acreditado_folio                VARCHAR(255) NOT NULL,
-    acreditado_municipio            VARCHAR(255) NOT NULL,
-    acreditado_localidad            VARCHAR(255) NOT NULL,
-    tipo_garantia                   VARCHAR(255) NOT NULL,
-    acreditado_garantia             VARCHAR(255) NOT NULL,
-    acreditado_telefono             VARCHAR(255) NOT NULL,
-    acreditado_email                VARCHAR(255) NOT NULL,
-    acreditado_direccion_negocio    VARCHAR(255) NOT NULL,
-    acreditado_direccion_particular VARCHAR(255) NOT NULL,
-    aval_nombre                     VARCHAR(255),
-    aval_telefono                   VARCHAR(255),
-    aval_email                      VARCHAR(255),
-    aval_direccion                  VARCHAR(255),
-    gestion_fecha1                  VARCHAR(255) DEFAULT '',
-    gestion_via1                    VARCHAR(255) DEFAULT '',
-    gestion_comentarios1            VARCHAR(255) DEFAULT '',
-    gestion_contador                INT          NOT NULL,
-    evidencia_fecha1                VARCHAR(255) DEFAULT '',
-    evidencia_fotografia1           VARCHAR(255) DEFAULT '',
-    evidencia_contador              INT          NOT NULL,
-    nombre_archivo                  VARCHAR(255) NOT NULL
-);
+    id                              INT(11)                                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fecha_creacion                  TIMESTAMP                               NOT NULL,
+    acreditado_nombre               VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_folio                VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_municipio            VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_localidad            VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    tipo_garantia                   VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_garantia             VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_telefono             VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_email                VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_direccion_negocio    VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    acreditado_direccion_particular VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    aval_nombre                     VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    aval_telefono                   VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    aval_email                      VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    aval_direccion                  VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    nombre_archivo                  VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE gestion
+(
+    id                   INT(11)                                 NOT NULL AUTO_INCREMENT,
+    gestion_fecha        VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    gestion_via          VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    gestion_comentarios  VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    evidencia_fecha      VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    evidencia_fotografia VARCHAR(254) COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+    bitacora_id          INT(11)                                 NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (bitacora_id) REFERENCES bitacora (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE usuario
 (
-    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre   VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+    id       INT(11)                                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre   VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL,
+    password VARCHAR(254) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 INSERT INTO usuario
 VALUES (null, 'Admin', '123456789@MY');
