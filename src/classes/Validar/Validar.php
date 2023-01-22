1af3597937a01f9285d60d4a8de61ecb41ca5a02
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-22 13:39:57 -0600

    Add static method to validate password

--- src/classes/Validar/Validar.php
+++ src/classes/Validar/Validar.php
@@ -30,6 +30,18 @@ class Validar
         return filter_var($email, FILTER_VALIDATE_EMAIL) ? true : false;
     }
 
+    public static function esPassword($password): bool
+    {
+        if (mb_strlen($password) >= 8
+            and preg_match('/[0-9]/', $password)
+            and preg_match('/[A-Z]/', $password)
+            and preg_match('/[@]/', $password)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
     public static function esNumeroExpediente($numero_expediente): bool
     {
         return preg_match('/(^IYE{1,1})([\d\-]+$)/', $numero_expediente) ? true : false;
