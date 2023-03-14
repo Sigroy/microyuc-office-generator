42cd6936ec432051e568eba9bc2c2f6a9d8c2733
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-03-14 16:41:14 -0600

    Finish the script to generate the excel file for the letters report

--- src/pages/excel-cartas.php
+++ src/pages/excel-cartas.php
@@ -1,16 +1,11 @@
 <?php
-require './config/config.php';
-require './includes/functions.php';
-require './includes/SimpleXLSXGen.php';
-
-check_login();
+declare(strict_types=1);
 
 $tz_CDMX = new DateTimeZone('America/Mexico_City');
 $CDMX = new DateTime('now', $tz_CDMX);
 $fecha_actual = $CDMX->format('d-m-Y');
 
-$cartas = [];
-$cartas = [
+$excel = [
     ['<b>N.°</b>', '<b>Fecha de creación</b>', '<b>Fecha de visita</b>', '<b>Folio</b>', '<b>Nombre</b>',
         '<b>Colonia/Fraccionamiento', '<b>Localidad</b>', '<b>Municipio</b>', '<b>Fecha de firma de anexos</b>',
         '<b>Documentación</b>', '<b>Monto de comprobación</b>', '<b>Tipo de comprobación</b>',
@@ -19,33 +14,37 @@ $cartas = [
         '<b>Mensualidades vencidas</b>', '<b>Adeudo total</b>',],
 ];
 
-$sql = 'SELECT * FROM carta;';
-$res = mysqli_query($conn, $sql);
-if (mysqli_num_rows($res) > 0) {
-    foreach ($res as $row) {
-        $cartas[] = array_values($row);
+$cartas = $cms->getCarta()->getAll();
+
+if ($cartas === false) {
+    redirect('cartas', ['error' => 'Hubo un error al generar el archivo de Excel']);
+    exit;
+}
+
+if (count($cartas) > 0) {
+    foreach ($cartas as $carta) {
+        $excel[] = array_values($carta);
     }
 }
 
-for ($i = 1; $i < count($cartas); $i++) {
-    $cartas[$i][0] = '<b>' . $i . '</b>';
-    $cartas[$i][1] = date('d-m-Y H:i:s', strtotime($cartas[$i][1]));
-    $cartas[$i][2] = $cartas[$i][2] ? date('d-m-Y', strtotime($cartas[$i][2])) : '';
-    $cartas[$i][11] = date('d-m-Y', strtotime($cartas[$i][11]));
-    $cartas[$i][13] = number_format($cartas[$i][13], 2);
-    $cartas[$i][14] = ucfirst($cartas[$i][14]);
-    $cartas[$i][15] = date('m-Y', strtotime($cartas[$i][15]));
-    $cartas[$i][16] = date('m-Y', strtotime($cartas[$i][16]));
-    $cartas[$i][19] = date('d-m-Y', strtotime($cartas[$i][19]));
-    $cartas[$i][20] = number_format($cartas[$i][20], 2);
-    $cartas[$i][22] = number_format($cartas[$i][22], 2);
-    unset($cartas[$i][5]);
-    unset($cartas[$i][6]);
-    unset($cartas[$i][7]);
-    unset($cartas[$i][23]);
+for ($i = 1; $i < count($excel); $i++) {
+    $excel[$i][0] = '<b>' . $i . '</b>';
+    $excel[$i][1] = date('d-m-Y H:i:s', strtotime($excel[$i][1]));
+    $excel[$i][2] = $excel[$i][2] ? date('d-m-Y', strtotime($excel[$i][2])) : '';
+    $excel[$i][11] = date('d-m-Y', strtotime($excel[$i][11]));
+    $excel[$i][13] = number_format((float)$excel[$i][13], 2);
+    $excel[$i][14] = ucfirst($excel[$i][14]);
+    $excel[$i][15] = date('m-Y', strtotime($excel[$i][15]));
+    $excel[$i][16] = date('m-Y', strtotime($excel[$i][16]));
+    $excel[$i][19] = date('d-m-Y', strtotime($excel[$i][19]));
+    $excel[$i][20] = number_format((float)$excel[$i][20], 2);
+    $excel[$i][22] = number_format((float)$excel[$i][22], 2);
+    unset($excel[$i][5]);
+    unset($excel[$i][6]);
+    unset($excel[$i][7]);
+    unset($excel[$i][23]);
 }
 
 $filename = 'Reporte de cartas ' . $fecha_actual . '.xlsx';
 
-$xlsx = Shuchkin\SimpleXLSXGen::fromArray($cartas);
-$xlsx->downloadAs($filename);
+$xlsx = Shuchkin\SimpleXLSXGen::fromArray($excel)->downloadAs($filename);
