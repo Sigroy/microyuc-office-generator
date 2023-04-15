7923f7be33142e111bcc93c1c79fb3565e94d4cb
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-04-14 21:03:59 -0600

    Add functionaly to the script to remove logs so that it all removes the evidence photos stored on the server

--- src/pages/eliminar-bitacora.php
+++ src/pages/eliminar-bitacora.php
@@ -5,15 +5,27 @@ if (!$id) {
     redirect('bitacoras');
 } else {
 
+    $gestiones = $cms->getGestion()->getByBitacoraId($id);
+
+    foreach ($gestiones as $gestion) {
+        $evidencias[] = $cms->getEvidencia()->getByGestionId($gestion['id']);
+    }
+
     // Consulta para recibir el nombre del archivo a eliminar
     $nombre_archivo = $cms->getBitacora()->getFileNameById($id);
 
-    // Sentencia para borrar la carta en la base de datos
+    // Sentencia para borrar la bitácora en la base de datos
     $eliminada = $cms->getBitacora()->delete($id);
 
     if ($eliminada === false || $eliminada === 0) {
         redirect('bitacoras/', ['error' => 'Hubo un error al eliminar la bitácora']);
         exit;
+    }
+
+    foreach ($evidencias as $evidencia) {
+        if (file_exists(UPLOADS . $evidencia['evidencia_fotografia'])) {
+            unlink(UPLOADS . $evidencia['evidencia_fotografia']);
+        }
     }
 
     unlink('./files/bitacoras/' . $nombre_archivo);
