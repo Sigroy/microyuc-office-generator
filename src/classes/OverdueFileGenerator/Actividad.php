8fa60e242d05ebae860882066db68f45e700a887
Sigroy <70609482+Sigroy@users.noreply.github.com>
2023-05-01 14:22:39 -0600

    Change order of the getAll query in the Activity table

--- src/classes/OverdueFileGenerator/Actividad.php
+++ src/classes/OverdueFileGenerator/Actividad.php
@@ -16,7 +16,8 @@ class Actividad
         try {
             $sql = "SELECT a.id, a.documento, a.accion, a.cliente, a.fecha_hora, a.usuario_id, u.nombre AS nombre_usuario
                     FROM actividad AS a
-                    INNER JOIN usuario AS u ON a.usuario_id = u.id;";
+                    INNER JOIN usuario AS u ON a.usuario_id = u.id
+                    ORDER BY a.fecha_hora DESC";
             return $this->db->runSQL($sql)->fetchAll();
         } catch (\PDOException) {
             return false;
